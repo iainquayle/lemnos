0f8a01cb69ecb45e59ba04475927fe8061ae734f
iainquayle <36175014+iainquayle@users.noreply.github.com>
2023-08-18 22:41:36 -0600

    fixing split node operations

--- src/tests/transition_split.py
+++ src/tests/transition_split.py
@@ -1,4 +1,20 @@
-from kontrol.transitions import Transition, TransitionGroup, ConvTransition
+from kontrol.transitions import Transition, TransitionGroup, SplitTreeNode 
 
 def test():
-	pass
+	print("testing splits nodes")
+	t1 = Transition()
+	t2 = Transition()
+	t3 = Transition()
+	
+	tg1 = TransitionGroup({t2: True, t3: True})	
+	tg2 = TransitionGroup({t3: True})
+
+	stn1 = SplitTreeNode()
+	stn1.add(tg1, t2)
+	stn2 = SplitTreeNode()
+	stn2.add(tg1, t3)
+	stn3 = SplitTreeNode()
+	stn3.add(tg1, t3, stn1)
+	stn2.merge(stn3)
+	
+	print(stn2)
