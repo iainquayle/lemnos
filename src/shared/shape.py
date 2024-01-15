206ee29dc7867b2dd8c8fdc3af3b81b88a40d1e3
iain <iainquayle@outlook.com>
2024-01-15 02:09:21 -0700

    finish shape

--- src/shared/shape.py
+++ src/shared/shape.py
@@ -7,32 +7,37 @@ from copy import copy
 from torch import Size
 from math import prod
 
-#TODO: consider using tuple instead, like size does
-#or using list, then get the already implemented functionality like the iterator...
-#or other option is build on top of size itself, though thats lots of abstraction
-class Shape(tuple):
+#not extending:
+#	size, as it is just tuple
+#	tuple, because init doesnt play nice when adding other parameters, and makes manipulation harder
+#TODO: probably want to optim some of this
+class Shape:
 	def __init__(self, shape: Tuple[int, ...] | List[int] | Size, fixed: bool = True) -> None:
-		super().__new__(cls(shape))
+		self.shape: List[int] = list(shape) if not isinstance(shape, List) else shape
 		self.fixed: bool = fixed
-	#TODO: find better name for upper
+	@staticmethod
+	def new_fixed(shape: Tuple[int, ...] | List[int] | Size) -> Shape:
+		return Shape(shape, True)
+	@staticmethod
+	def new_unfixed(shape: Tuple[int, ...] | List[int] | Size) -> Shape:
+		return Shape(shape, False)
 	def upper_length(self) -> int:
 		return len(self.shape) - 1 if self.fixed else 0
-	def reverse_upper_shape(self, reverse_index: int) -> List[int]:
-		return self.shape[-reverse_index:]
+	def reverse_upper_equal(self, reverse_index: int, other: Self) -> bool:
+		return self.shape[-reverse_index:] == other.shape[-reverse_index:]
 	def reverse_lower_product(self, reverse_index: int) -> int:
 		return prod(self.shape[:-reverse_index])
 	def dimensions(self) -> int:
 		return len(self.shape) + 1 if not self.fixed else 0
 	def upper_compatible(self, other: Self) -> bool:
 		reverse_index = min(self.upper_length(), other.upper_length())
-		return self.shape[-reverse_index:] == other.shape[-reverse_index:] 
+		return self.reverse_upper_equal(reverse_index, other)
 	def to_fixed(self, dimension: int) -> Shape:
-		return Shape([dimension] + self.shape, True)
-	def to_size(self) -> Size:
-		if self.fixed:
-			return Size(self.shape)
-		else:
-			return Size([-1] + self.shape)
+		return Shape(([] if self.fixed else [dimension]) + self.shape, True)
+	def to_unfixed(self) -> Shape:
+		return Shape(self.shape[1 if self.fixed else 0:], False)
+	def to_tuple(self) -> Tuple[int, ...]:
+		return tuple(([] if self.fixed else [-1]) + self.shape)
 	#rules:
 	#	if no remaining open dims
 	#		dims to the right must be the same, dims to the left must be prod the same
@@ -56,7 +61,7 @@ class Shape(tuple):
 				if big_product % small.shape[0] != 0:
 					return None
 				common = big.to_fixed(big_product // small.shape[0])
-		return None if small.reverse_upper_shape(reverse_index) != big.reverse_upper_shape(reverse_index) else common
+		return None if big.reverse_upper_equal(reverse_index, small) else common
 	def common_small(self, other: Self) -> Shape | None:
 		self_smaller = len(self) < len(other) or (len(self) == len(other) and self.dimensions() < other.dimensions())
 		small = self if self_smaller else other 
@@ -69,7 +74,7 @@ class Shape(tuple):
 		else:
 			if big.fixed:
 				common = small.to_fixed(big.reverse_lower_product(reverse_index))
-		return None if small.reverse_upper_shape(reverse_index) != big.reverse_upper_shape(reverse_index) else common
+		return None if big.reverse_upper_equal(reverse_index, small) else common
 	@staticmethod
 	def reduce_common_large(shapes: Iterable[Shape]) -> Shape | None:
 		shapes_iter = iter(shapes)
@@ -93,29 +98,25 @@ class Shape(tuple):
 		return not isinstance(other, Self) and self.shape == other.shape and self.fixed == other.fixed
 
 class Bound:
-	def __init__(self, lower: Size | List[int] | int = Size(), upper: Size | List[int] | int = Size()) -> None:
-		lower = Size([lower]) if isinstance(lower, int) else Size(lower)
-		upper = Size([upper]) if isinstance(upper, int) else Size(upper)
-		if len(lower) != len(upper):
-			raise Exception("bound dimensions do not match")
-		for lower_bound, upper_bound in zip(lower, upper):
-			if lower_bound > upper_bound:
-				raise Exception("lower bound greater than upper")
-			if lower_bound <= 0:
+	def __init__(self, bounds: List[Tuple[int, int]]) -> None:
+		for i in range(len(bounds)):
+			if bounds[i][0] > bounds[i][1]:
+				bounds[i] = (bounds[i][1], bounds[i][0])
+			if bounds[i][0] <= 0:
 				raise Exception("lower bound less than 1")
-		self.upper: Size = upper
-		self.lower: Size = lower 
-	def __contains__(self, shape: Size) -> bool:
-		if len(shape) != len(self.lower):
+		self.bounds: List[Tuple[int, int]] = bounds
+	def __contains__(self, shape: Shape) -> bool:
+		if shape.dimensions() != len(self.bounds):
 			return False
-		for lower_bound, upper_bound, i in zip(self.lower, self.upper, shape):
-			if i < lower_bound or i > upper_bound:
+		#TODO: probably nicer way to do this, dont really like using the -1
+		for (lower, upper), i in zip(self.bounds, shape.to_tuple()):
+			if i != -1 and (i < lower or i > upper):
 				return False
 		return True
 	def __len__(self) -> int:
-		return len(self.lower)
+		return len(self.bounds)
 	def __str__(self) -> str:
-		return f"Bound({self.lower}, {self.upper})"
+		return f"Bound({self.bounds})"
 	def __repr__(self) -> str:
 		return str(self)
 
