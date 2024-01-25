98c6c13f577ba035ac1ee0807d21758eb49708da
iain <iainquayle@outlook.com>
2024-01-24 23:47:47 -0700

    work on node build

--- src/pattern/priority_graphs/manual.py
+++ src/pattern/priority_graphs/manual.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 from src.pattern.node_parameters import BaseParameters, IdentityParameters 
-from src.shared.shape import Bound, LockedShape, Shape
+from src.shared.shape import Bound, LockedShape, Shape, OpenShape
 from src.shared.index import Index
 from src.shared.merge_method import MergeMethod, Concat
 from src.model.graph import Node, Graph as ModelGraph
@@ -21,10 +21,11 @@ class _ExpansionNode:
 		self.priority: int
 	def get_parent_shapes(self) -> List[LockedShape]:
 		return [parent.get_output_shape() for parent in self.parents]
-	def add_parent(self, parent: Node) -> None:
+	def add_parent(self, parent: Node, priority: int) -> None:
 		if self.taken(parent):
 			raise ValueError("Parent already taken")
 		self.parents.append(parent)
+		self.priority = min(self.priority, priority) 
 	def taken(self, node: Node) -> bool:
 		for parent in self.parents:
 			if parent.get_pattern() == node.get_pattern():
@@ -53,19 +54,19 @@ class _ExpansionStack:
 		return len(self._stack)
 	def __copy__(self) -> _ExpansionStack:
 		return _ExpansionStack(copy(self._stack))
-def _min_expansion_pattern(expansion_nodes: Dict[NodePattern, _ExpansionStack]) -> NodePattern | None:
+def _min_expansion(expansion_nodes: Dict[NodePattern, _ExpansionStack]) -> NodePattern | None:
 	return min(expansion_nodes.items(), key=lambda item: item[1].get_priority())[0]
 
 class Graph:
 	def __init__(self) -> None:
-		if len(self._start_patterns) == 0 or len(self._end_patterns) == 0:
+		if len(self._starts) == 0 or len(self._ends) == 0:
 			raise ValueError("No start or end patterns")
-		self._start_patterns: List[NodePattern] = []
-		self._end_patterns: List[NodePattern] = []
-	def add_start_pattern(self, pattern: NodePattern) -> None:
-		self._start_patterns.append(pattern)
-	def add_end_pattern(self, pattern: NodePattern) -> None:
-		self._end_patterns.append(pattern)
+		self._starts: List[NodePattern] = []
+		self._ends: List[NodePattern] = []
+	def add_start(self, pattern: NodePattern) -> None:
+		self._starts.append(pattern)
+	def add_end(self, pattern: NodePattern) -> None:
+		self._ends.append(pattern)
 	#TODO: perhaps add a flag that switches whether the indices should be attempted in order, or just used at random for breeding,
 	#	could use a random seed, that can also effectivley work as a flag
 	#only real option for capturing input and output nodes in current setup is to return a list of nodes and find them after
@@ -74,22 +75,27 @@ class Graph:
 	#	since rn inputs dont have priorities, meaning the creator of a node wont necessarily go first
 	#	or just try finding the true start pattern
 	def build(self, input_shapes: List[LockedShape], output_shapes: List[LockedShape], indices: List[Index]) -> None:
-		if len(input_shapes) != len(self._start_patterns):
+		if len(input_shapes) != len(self._starts):
 			raise ValueError("Incorrect number of input shapes")
 		expansion_nodes: Dict[NodePattern, _ExpansionStack] = {}
 		input_nodes: List[Node] = []
-		for i, (pattern, shape) in enumerate(zip(self._start_patterns, input_shapes)):
-			input_pattern = NodePattern(IdentityParameters(Bound([None] * len(shape))), Concat())
-			input_node = Node(Index(), i, input_pattern, shape, shape, None)
+		for i, (pattern, shape) in enumerate(zip(self._starts, input_shapes)):
+			input = NodePattern(IdentityParameters(Bound([None] * len(shape))), Concat())
+			input_node = Node(Index(), i, input, shape, shape, None)
 			input_nodes.append(input_node)
-			expansion_nodes[pattern] = _ExpansionStack([_ExpansionNode({input_pattern: input_node}, -1)])
-		chosen_pattern: NodePattern | None = _min_expansion_pattern(expansion_nodes) 
-		if chosen_pattern is None:
+			expansion_nodes[pattern] = _ExpansionStack([_ExpansionNode([input_node], -1)])
+		chosen: NodePattern | None = _min_expansion(expansion_nodes) 
+		if chosen is None:
 			raise ValueError("No valid start pattern")	
 		else: 
-			chosen_pattern.build(expansion_nodes, indices, len(input_nodes))
+			chosen.build(expansion_nodes, indices, len(input_nodes))
 	def _build_node(self, expansion_nodes: Dict[NodePattern, _ExpansionStack], indices: List[Index], id: int) -> List[Node] | NodePattern:
-		return []	
+		chosen: NodePattern | None = _min_expansion(expansion_nodes)
+		if chosen is None:
+			return []
+		else:
+
+			return []	
 		
 
 class NodePattern:
@@ -100,35 +106,64 @@ class NodePattern:
 		self._merge_method: MergeMethod = merge_method 
 	def add_transition_group(self, group: TransitionGroup) -> None:
 		self._transition_groups.append(copy(group))
-	#TODO: condsider moving recursive step to graph, makes more sense 
+	#procedure
+	#	iterate over groups
+	#		check immediate constraints 
+	#			expand node
+	#			recurse
+	#			if fail comes back, try next group
+	#		if all fail, return node pattern 
+	#			maybe node itself? would node pattern possibly destroy whole graph?
+	#			it shouldnt, as the node that expanded was at the top of the stack
+	#			and any node invalidated by it would have been removed on the way back up apart from the original creator
 	def build(self, expansion_nodes: Dict[NodePattern, _ExpansionStack], indices: List[Index], id: int) -> List[Node] | NodePattern:
 		index = indices[0]
-		parents: Iterable[Node] = expansion_nodes[self].pop().parents.values()
+		parents: Iterable[Node] = expansion_nodes[self].pop().parents
 		input_shape = self._merge_method.get_output_shape([parent.get_output_shape() for parent in parents])
-		valid_group_and_shapes: Tuple[TransitionGroup, Shape, Shape] | None = None
+		pivot = index.to_int(len(self._transition_groups))
+		i = 0
+		outside = 0
+		while outside < 2: 
+			if pivot + i < len(self._transition_groups) and pivot + i >= 0:
+				group = self._transition_groups[pivot + i]
+				outside = 0
+				#maybe just make a new func that deals with expanding a certain group, and simplify this shit, pain in the ass
+			else:
+				outside += 1
+			i = -i if i > 0 else -i + 1
+
+
+		valid_group_and_shapes: Tuple[TransitionGroup, Dict[Transiton, _ExpansionNode], Shape, Shape] | None = None
 		group_iter = iter(self._transition_groups)
-		while (group := next(group_iter, None)) is not None:
-			conformance_shapes: List[Shape] = [] 
-			for transition in group._transitions:
-				if transition.join_existing:
-					conformance_shapes.append(transition.next_pattern.get_conformance_shape(expansion_nodes[transition.next_pattern].peek().get_parent_shapes()))
-			if (conformance_shape := Shape.reduce_common_lossless(conformance_shapes)) is not None:
+		while (group := next(group_iter, None)) is not None: #add cut off once reached past index point
+			conformance_shape = OpenShape.new()
+			join_nodes: Dict[Transiton, _ExpansionNode] = {}
+			transition_iter = iter(group._transitions)
+			while (transition := next(transition_iter, None)) is not None and conformance_shape is not None:
+				if transition.join_existing and ((join_on := expansion_nodes[transition.next].get_available(parents[0])) is not None
+						and (conformance_shape := conformance_shape.common_lossless(transition.next.get_conformance_shape(join_on.get_parent_shapes()))) is not None):
+					join_nodes[transition] = join_on
+				else:
+					conformance_shape = None #bit of a hack but technically true
+			if conformance_shape is not None:
 				shapes = self._node_parameters.get_mould_and_output_shapes(input_shape, conformance_shape, index)
 				if shapes is not None:
-					valid_group_and_shapes = (group, *shapes)
+					valid_group_and_shapes = (group, join_nodes, *shapes)
 		if valid_group_and_shapes is None:
-			return self
+			return self 
 		else:
-			node = Node(index, id, self, valid_group_and_shapes[1], valid_group_and_shapes[2], parents)
+			group, join_nodes, mould_shape, output_shape = valid_group_and_shapes
+			node = Node(index, id, self, mould_shape, output_shape, parents)
 			for transition in valid_group_and_shapes[0]._transitions:
+				stack = expansion_nodes[transition.next]
 				if transition.join_existing:
-					
-					pass
+					join_nodes[transition].add_parent(node, transition.priority)
 				else:
-					if transition.next_pattern not in expansion_nodes:
-						expansion_nodes[transition.next_pattern] = _ExpansionStack()
-					expansion_nodes[transition.next_pattern].push(_ExpansionNode({self: node}, transition.priority))
-			return self
+					if transition.next not in expansion_nodes:
+						expansion_nodes[transition.next] = _ExpansionStack()
+					stack.push(_ExpansionNode([node], transition.priority))
+			return [node]
+	#def _build_group(self, group_index: int)
 	def get_conformance_shape(self, input_shapes: List[LockedShape]) -> Shape:
 		return self._merge_method.get_conformance_shape(input_shapes)
 	@abstractmethod	
@@ -139,12 +174,12 @@ class NodePattern:
 class Transiton:
 	_MAX_PRIORITY: int = 128 
 	_MIN_PRIORITY: int = 0 
-	__slots__ = ["next_pattern", "optional", "priority", "join_existing"]
-	def __init__(self, next_pattern: NodePattern, optional: bool = False, priority: int = 0, join_existing: bool = False) -> None:
+	__slots__ = ["next", "optional", "priority", "join_existing"]
+	def __init__(self, next: NodePattern, priority: int, join_existing: bool = False) -> None:
 		if priority > Transiton._MAX_PRIORITY or priority < Transiton._MIN_PRIORITY:
 			raise ValueError("Priority out of bounds")
-		self.next_pattern: NodePattern = next_pattern
-		self.optional: bool = optional 
+		self.next: NodePattern = next
+		self.optional: bool =  False #optional 
 		self.priority: int = priority 
 		self.join_existing: bool = join_existing 
 	@staticmethod
@@ -162,9 +197,9 @@ class TransitionGroup:
 	def set_transitions(self, transitions: List[Transiton]) -> None:
 		pattern_set: Set[NodePattern] = set()
 		for transition in transitions:
-			if transition.next_pattern in pattern_set:
+			if transition.next in pattern_set:
 				raise ValueError("Duplicate state in transition group")
-			pattern_set.add(transition.next_pattern)
+			pattern_set.add(transition.next)
 		self._transitions = transitions
 	def __str__(self) -> str:
 		return f"TG{self._transitions}"
