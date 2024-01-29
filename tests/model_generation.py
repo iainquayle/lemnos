043c769ee99c6ff890bcafc0caef2dcd591845a1
iain <iainquayle@outlook.com>
2024-01-29 02:04:53 -0700

    small rename, more tests

---
+++ tests/model_generation.py
@@ -0,0 +1,9 @@
+import unittest
+
+from src.model.model import ModelNode, Model 
+from src.schema.priority_graphs.manual import SchemaNode, Transition, TransitionGroup, Schema
+from src.shared.shape import Bound, LockedShape 
+
+class TestModelLinking(unittest.TestCase):
+	pass
+
