d2f59ebc8ff24eb72943b54244f949c27b3504bf
iain <iainquayle@outlook.com>
2024-03-07 00:14:35 -0700

    control work before bricking system

--- src/control/control.py
+++ src/control/control.py
@@ -8,13 +8,15 @@ from torch.utils.data import Dataset, DataLoader
 from torch.nn import Module
 import torch
 
-from typing import List, Tuple
+from typing import List, Tuple, Any
 from enum import Enum
 from dataclasses import dataclass
 
 class OptimizerType(Enum):
 	ADAM = "adam"
 	SGD = "sgd"
+
+REQUIRE_CUDA = True
 
 CUDA = "cuda"
 CPU = "cpu"
@@ -41,6 +43,8 @@ class Control:
 		) -> None:
 		#look into taking in a sampler for the data loader, may be useful for large datasets
 		device_type = CUDA if torch.cuda.is_available() else CPU 
+		if REQUIRE_CUDA and not device_type == CUDA:
+			raise ValueError("CUDA set to required but not available")
 		device = torch.device(device_type)
 		train_loader = DataLoader(self._train_dataset, batch_size=batch_size, shuffle=True, num_workers=workers, persistent_workers=workers > 0, pin_memory=True)
 		validation_loader = DataLoader(self._validation_dataset, batch_size=1, shuffle=True, num_workers=0, pin_memory=True)
@@ -60,11 +64,13 @@ class Control:
 				if COMPILE_MODELS:
 					runnable_model = torch.compile(runnable_model, backend=COMPILER_BACKEND) 
 				runnable_model.to(device)
+				optimizer = get_optimizer(optimizer_type, runnable_model)
 				epochs = 1
 				for _ in range(1):
 					runnable_model.train()
-					for epoch in range(epochs):
-						optimizer = torch.optim.Adam(runnable_model.parameters())
+					training_stats: PerformanceStats = PerformanceStats()
+					for _ in range(epochs):
+						training_stats = PerformanceStats()
 						scaler = torch.cuda.amp.GradScaler()
 						for data, truth in train_loader:
 							data, truth = data.to(device), truth.to(device)
@@ -72,20 +78,25 @@ class Control:
 							with torch.autocast(device_type=device_type, dtype=torch.float16):
 								output = runnable_model(data)
 								loss = criterion(output, truth)
+								training_stats.record(loss.item(), 0)
 							scaler.scale(loss).backward()
 							scaler.step(optimizer)
 							scaler.update()
 					runnable_model.eval()
+					model_pool[-1][1].append(training_stats)
+					validation_stats: PerformanceStats = PerformanceStats()
 					for data, truth in validation_loader:
 						data, truth = data.to(device), truth.to(device)
 						with torch.no_grad():
 							output = runnable_model(data)
 							loss = criterion(output, truth)
+							validation_stats.record(loss.item(), 0)
+					model_pool[-1][2].append(validation_stats)
 
 def set_learning_rate(optimizer: torch.optim.Optimizer, learning_rate: float) -> None:
 	for param_group in optimizer.param_groups:
 		param_group["lr"] = learning_rate
-def get_optimizer(optimizer_type: OptimizerType, model: Module) -> torch.optim.Optimizer:
+def get_optimizer(optimizer_type: OptimizerType, model: Any) -> torch.optim.Optimizer:
 	if optimizer_type == OptimizerType.ADAM:
 		return torch.optim.Adam(model.parameters())
 	elif optimizer_type == OptimizerType.SGD:
@@ -109,5 +120,7 @@ class PerformanceStats:
 		self._correct_total += correct 
 	def get_average_loss(self) -> float:
 		return self._loss_total / self._samples
-	def get_accuracy(self) -> float:	
-		return self._correct_total / self._samples
+	#this will need to have a user defined function to calculate whether something is correct or not 
+	#thus will leave it alone rn
+	#def get_accuracy(self) -> float:	
+	#	return self._correct_total / self._samples
