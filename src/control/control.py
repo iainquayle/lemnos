8b07847dd713a08c601bab59c782d88625ee114f
iainquayle <iainquayle@outlook.com>
2024-03-10 22:45:51 -0600

    start moving build to model

--- src/control/control.py
+++ src/control/control.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 from torch.nn import Module
 import torch
 
-from typing import List, Tuple, Any
+from typing import List, Dict, Any
 from enum import Enum
 from dataclasses import dataclass
 
@@ -23,7 +23,14 @@ CUDA = "cuda"
 CPU = "cpu"
 
 class Control:
-	def __init__(self, schema: Schema, train_dataset: Dataset, validation_dataset: Dataset, compile_models: bool = True, compiler_backend: CompileBackend = CompileBackend.INDUCTOR, require_cuda: bool = True) -> None:
+	def __init__(self, 
+			schema: Schema, 
+			train_dataset: Dataset, 
+			validation_dataset: Dataset, 
+			compile_models: bool = True, 
+			compiler_backend: CompileBackend = CompileBackend.INDUCTOR, 
+			require_cuda: bool = True
+			) -> None:
 		self._schema: Schema = schema
 		self._train_dataset: Dataset = train_dataset
 		self._validation_dataset: Dataset = validation_dataset
@@ -54,58 +61,83 @@ class Control:
 			else:
 				raise ValueError("Model could not be built from schema")
 		#could make the performance stats hold the model instead?
-		model_pool: List[Tuple[Model, List[PerformanceStats], List[PerformanceStats]]] = []
+		model_pool: Dict[Model, ModelMetrics] = {} 
 		breed_iterations = 1
-		#move train and eval loops into functions
+		training_epochs = 1
 		#then wrap the pool in a function too?
-		for i in range(breed_iterations): #will switch this to use a call back? allowing for a cli?
+		i = 0
+		while i < breed_iterations: #will switch this to use a call back? allowing for a cli?
 			for model in test_models:
-				model_pool.append((model, [], []))
+				model_pool[model] = ModelMetrics()
 				runnable_model: Any = model.get_torch_module_handle(f"M{i}")() #fix any type
 				if self._compile_models:
 					runnable_model = torch.compile(runnable_model, backend=str(self._compiler_backend)) 
 				runnable_model.to(device)
 				optimizer = get_optimizer(optimizer_type, runnable_model)
-				epochs = 1
 				for _ in range(1):
-					runnable_model.train()
-					training_stats: PerformanceStats = PerformanceStats()
-					for _ in range(epochs):
-						training_stats = PerformanceStats()
-						scaler = torch.cuda.amp.GradScaler()
-						for data, truth in train_loader:
-							data, truth = data.to(device), truth.to(device)
-							optimizer.zero_grad(set_to_none=True)
-							with torch.autocast(device_type=device_type, dtype=torch.float16):
-								output = runnable_model(data)
-								loss = criterion(output, truth)
-								training_stats.record(loss.item(), 0)
-							scaler.scale(loss).backward()
-							scaler.step(optimizer)
-							scaler.update()
-					runnable_model.eval()
-					model_pool[-1][1].append(training_stats)
-					validation_stats: PerformanceStats = PerformanceStats()
-					for data, truth in validation_loader:
-						data, truth = data.to(device), truth.to(device)
-						with torch.no_grad():
-							output = runnable_model(data)
-							loss = criterion(output, truth)
-							validation_stats.record(loss.item(), 0)
-					model_pool[-1][2].append(validation_stats)
+					for _ in range(training_epochs):
+						model_pool[model].record_training_epoch(train_epoch(runnable_model, criterion, optimizer, train_loader, device, device_type))
+					model_pool[model].record_validation_epoch(validate_epoch(runnable_model, criterion, validation_loader, device, device_type))
 
-def set_learning_rate(optimizer: torch.optim.Optimizer, learning_rate: float) -> None:
-	for param_group in optimizer.param_groups:
-		param_group["lr"] = learning_rate
-def get_optimizer(optimizer_type: OptimizerType, model: Any) -> torch.optim.Optimizer:
-	if optimizer_type == OptimizerType.ADAM:
-		return torch.optim.Adam(model.parameters())
-	elif optimizer_type == OptimizerType.SGD:
-		return torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-	else:
-		raise ValueError("Invalid optimizer type")
+			i += 1
+def cull_models(model_pool: Dict[Model, ModelMetrics], max_pool_size: int) -> None:
+	#make this more capable later
+	#makeing the model pool keep track of order would make this easier
+	#either way will need to sort the performances
+	cutoff: float = 0.0
+	
+	pass
 
-class PerformanceStats:
+def train_epoch(model: Module, criterion: Module, optimizer: torch.optim.Optimizer, train_loader: DataLoader, device: torch.device, device_type: str) -> EpochMetrics:
+	metrics: EpochMetrics = EpochMetrics()
+	model.train()
+	scaler = torch.cuda.amp.GradScaler()
+	for data, truth in train_loader:
+		data, truth = data.to(device), truth.to(device)
+		optimizer.zero_grad(set_to_none=True)
+		with torch.autocast(device_type=device_type, dtype=torch.float16):
+			output = model(data)
+			loss = criterion(output, truth)
+			metrics.record(loss.item(), 0)
+		scaler.scale(loss).backward()
+		scaler.step(optimizer)
+		scaler.update()
+	return metrics 
+def validate_epoch(model: Module, criterion: Module, validation_loader: DataLoader, device: torch.device, device_type: str) -> EpochMetrics:
+	metrics: EpochMetrics = EpochMetrics()
+	model.eval()
+	with torch.no_grad():
+		for data, truth in validation_loader:
+			data, truth = data.to(device), truth.to(device)
+			with torch.autocast(device_type=device_type, dtype=torch.float16):
+				output = model(data)
+				loss = criterion(output, truth)
+				metrics.record(loss.item(), 0)
+	return metrics 
+
+
+class ModelMetrics:
+	__slots__ = ["_train", "_validation"]
+	def __init__(self) -> None:
+		self._train: List[EpochMetrics] = []
+		self._validation: List[EpochMetrics] = []
+	def record_training_data(self, epoch: int, loss: float, correct: float, samples: int = 1) -> None: #may splits eqoch
+		if len(self._train) <= epoch:
+			self._train.append(EpochMetrics())
+		self._train[epoch].record(loss, correct, samples)
+	def record_validation_data(self, epoch: int, loss: float, correct: float, samples: int = 1) -> None:
+		if len(self._validation) <= epoch:
+			self._validation.append(EpochMetrics())
+		self._validation[epoch].record(loss, correct, samples)
+	def record_training_epoch(self, metrics: EpochMetrics) -> None:
+		self._train.append(metrics)
+	def record_validation_epoch(self, metrics: EpochMetrics) -> None:
+		self._validation.append(metrics)
+	def get_min_validation_loss(self) -> float:
+		return min([x.get_loss() for x in self._validation])
+	def get_min_training_loss(self) -> float:
+		return min([x.get_loss() for x in self._train])
+class EpochMetrics:
 	__slots__ = ["_loss_total", "loss_max", "loss_min", "_correct_total", "_samples"]
 	def __init__(self) -> None:
 		self._loss_total: float = 0
@@ -119,9 +151,20 @@ class PerformanceStats:
 		self.loss_max = max(self.loss_max, loss)
 		self.loss_min = min(self.loss_min, loss)
 		self._correct_total += correct 
-	def get_average_loss(self) -> float:
+	def get_loss(self) -> float:
 		return self._loss_total / self._samples
 	#this will need to have a user defined function to calculate whether something is correct or not 
 	#thus will leave it alone rn
 	#def get_accuracy(self) -> float:	
 	#	return self._correct_total / self._samples
+
+def set_learning_rate(optimizer: torch.optim.Optimizer, learning_rate: float) -> None:
+	for param_group in optimizer.param_groups:
+		param_group["lr"] = learning_rate
+def get_optimizer(optimizer_type: OptimizerType, model: Any) -> torch.optim.Optimizer:
+	if optimizer_type == OptimizerType.ADAM:
+		return torch.optim.Adam(model.parameters())
+	elif optimizer_type == OptimizerType.SGD:
+		return torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+	else:
+		raise ValueError("Invalid optimizer type")
--- tests/model_node.py
+++ tests/model_node.py
@@ -1,35 +1,39 @@
 import unittest
 
-from src.schema import SchemaNode, Concat 
+from src.schema import SchemaNode, Concat, Sum 
 from src.shared import Index, LockedShape, ShapeBound
 from src.model import ModelNode
 
-class TestModelNode(unittest.TestCase):
+class TestModelNodeGraphFunctions(unittest.TestCase):
 	def setUp(self) -> None:
-		self.m1 = ModelNode(Index(0), 0, SchemaNode(ShapeBound((1, 1)), Concat()))
-		self.m2 = ModelNode(Index(0), 1, SchemaNode(ShapeBound((1, 1)), Concat()))
-		self.m3 = ModelNode(Index(0), 2, SchemaNode(ShapeBound((1, 1)), Concat()))
-	def test_attempt_set_children(self) -> None:
-		pass
+		self.m1 = ModelNode(Index(0), 0, SchemaNode(ShapeBound(None, None), Sum()), LockedShape(1, 1), LockedShape(1, 1))
+		self.m2 = ModelNode(Index(0), 1, SchemaNode(ShapeBound(None, None), Concat()), LockedShape(1, 2), LockedShape(1, 2))
+		self.m3 = ModelNode(Index(0), 2, SchemaNode(ShapeBound(None, None), Concat()), LockedShape(2, 1), LockedShape(2, 1))
+	def test_attempt_join_children_valid(self) -> None:
+		self.assertTrue(self.m1.attempt_join_children([self.m2], Index(0)))
+		self.assertTrue(self.m3.attempt_join_children([self.m2], Index(0)))
+	def test_attempt_join_children_invalid(self) -> None:
+		self.assertTrue(self.m2.attempt_join_children([self.m1], Index(0)))
+		self.assertFalse(self.m3.attempt_join_children([self.m1], Index(0)))
 	def test_set_children(self) -> None:
 		self.m1._set_children([self.m2])
 		self.assertEqual(self.m1._children, [self.m2])
 		self.m1._set_children([self.m3])
 		self.assertEqual(self.m1._children, [self.m3])
 	def test_unbind_children(self) -> None:
-		self.m1.add_child(self.m2)
+		self.m1._add_child(self.m2)
 		self.m1.unbind_children()
 		self.assertEqual(self.m1._children, [])
 		self.assertEqual(self.m2._parents, [])
 	def test_unbind_parents(self) -> None:
-		self.m1.add_parent(self.m2)
+		self.m1._add_parent(self.m2)
 		self.m1.unbind_parents()
 		self.assertEqual(self.m1._parents, [])
 		self.assertEqual(self.m2._children, [])
 	def test_set_parents(self) -> None:
-		self.m1.set_parents([self.m2])
+		self.m1._set_parents([self.m2])
 		self.assertEqual(self.m1._parents, [self.m2])
 		self.assertEqual(self.m2._children, [self.m1])
-		self.m1.set_parents([self.m3])
+		self.m1._set_parents([self.m3])
 		self.assertEqual(self.m1._parents, [self.m3])
 		self.assertEqual(self.m3._children, [self.m1])
