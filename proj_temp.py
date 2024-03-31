dfeb4cf7882b2a8caa57859f440d57cef06011d9
iainquayle <iainquayle@outlook.com>
2024-03-31 07:09:02 +0000

    move to twitter

--- proj_temp.py
+++ proj_temp.py
@@ -26,6 +26,22 @@ class IMDBDataset(Dataset):
 				tensor[char_index][i] = 1.0
 		return tensor, self.labels[index]
 
+TWEET_LENGTH = 256 
+class TweetDataset(Dataset):
+	def __init__(self, path: str):
+		csv = pd.read_csv(path, encoding="ISO-8859-1")
+		self.data = [tweet[:TWEET_LENGTH] for tweet in csv["text"]]
+		self.labels = [Tensor([1 if sentiment == 4 else 0]) for sentiment in csv["sentiment"]]
+		print(self.labels[0])
+	def __len__(self) -> int:
+		return len(self.data)
+	def __getitem__(self, index: int) -> tuple[Tensor, Tensor]:
+		tensor = torch.zeros(CLASS_SIZE, TWEET_LENGTH, dtype=torch.float32)
+		for i, char in enumerate(self.data[index]):
+			if (char_index := char_to_class_index(char)) is not None:
+				tensor[char_index][i] = 1.0
+		return tensor, self.labels[index]
+
 punctuation = ".,:!?()=/\"' +-*@#$%&"
 replace_map = {
 	'á': 'a',
@@ -96,48 +112,55 @@ def char_to_class_index(char: str) -> int | None:
 
 print(CLASS_SIZE)
 
-dataset = IMDBDataset("data/imdb.csv")
+dataset = TweetDataset("data/twitter.csv")
 train, test = random_split(dataset, [int(len(dataset) * 0.9), len(dataset) - int(len(dataset) * 0.9)])
 del dataset
 
-start = SchemaNode(ShapeBound((5, 15), None), Sum(), 
-	Conv((0.1, 0.5), 1, 1), ReLU6(), BatchNormalization())
+def get_schema_a():
+	start = SchemaNode(ShapeBound((5, 15), None), Sum(), 
+		Conv((0.1, 0.5), 1, 1), ReLU6(), BatchNormalization())
 
-skip = SchemaNode(ShapeBound(None, (1, review_length)), Sum(), None, None, BatchNormalization())
-expand = SchemaNode(ShapeBound((16, 384), None), Sum(), 
-	Conv((1, 4), 1, 1), ReLU6(), BatchNormalization())
-depthwise = SchemaNode(ShapeBound(None, None), Sum(), 
-	Conv(1.0, 7, 1, 1, 3, 1), ReLU6(), BatchNormalization())
-shrink = SchemaNode(ShapeBound((16, 384), None), Sum(), 
-	Conv((0.25, 1), 1, 1), None, BatchNormalization())
+	skip = SchemaNode(ShapeBound(None, (1, review_length)), Sum(), None, None, BatchNormalization())
+	expand = SchemaNode(ShapeBound((16, 384), None), Sum(), 
+		Conv((1, 4), 1, 1), ReLU6(), BatchNormalization())
+	depthwise = SchemaNode(ShapeBound(None, None), Sum(), 
+		Conv(1.0, 7, 1, 1, 3, 1), ReLU6(), BatchNormalization())
+	shrink = SchemaNode(ShapeBound((16, 384), None), Sum(), 
+		Conv((0.25, 1), 1, 1), None, BatchNormalization())
 
-down_sample = SchemaNode(ShapeBound((16, 172), (1, review_length)), Sum(), 
-	Conv((0.20, 1.0), 4, 4), ReLU6(), BatchNormalization())
+	down_sample = SchemaNode(ShapeBound((16, 172), (1, review_length)), Sum(), 
+		Conv((0.20, 1.0), 2, 2), ReLU6(), BatchNormalization())
 
-end = SchemaNode(ShapeBound(1, 1), Sum(), Full((0.1, 10)), None, None)
+	end = SchemaNode(ShapeBound(1, 1), Sum(), Full((0.1, 10)), None, None)
 
-start.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
+	#start.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
+	start.add_group((skip, 1, JoinType.NEW))
 
-skip.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
-expand.add_group((depthwise, 0, JoinType.NEW))
-depthwise.add_group((shrink, 0, JoinType.NEW))
-shrink.add_group((skip, 0, JoinType.EXISTING))
+	skip.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
+	expand.add_group((depthwise, 0, JoinType.NEW))
+	depthwise.add_group((shrink, 0, JoinType.NEW))
+	shrink.add_group((skip, 0, JoinType.EXISTING))
 
-skip.add_group((down_sample, 0, JoinType.NEW))
-skip.add_group((end, 0, JoinType.NEW))
+	skip.add_group((down_sample, 0, JoinType.NEW))
+	skip.add_group((end, 0, JoinType.NEW))
 
-down_sample.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
-down_sample.add_group((down_sample, 0, JoinType.NEW))
-down_sample.add_group((end, 0, JoinType.NEW))
+	#down_sample.add_group((expand, 0, JoinType.NEW), (skip, 1, JoinType.NEW))
+	down_sample.add_group((skip, 1, JoinType.NEW))
+	down_sample.add_group((down_sample, 0, JoinType.NEW))
+	down_sample.add_group((end, 0, JoinType.NEW))
 
-schema = Schema([start], [end])
+	return Schema([start], [end])
 
-#ir = schema.compile_ir([LockedShape(CLASS_SIZE, review_length)], BreedIndices(), ID(300))
+def get_schema_b():
+	pass
+
+#ir = get_schema_a().compile_ir([LockedShape(CLASS_SIZE, TWEET_LENGTH)], BreedIndices(), ID(62))
 #if ir is not None:
 #	print(generate_torch_module("M", ir))
 
-control = Control(schema, train, test, compile_models=False, max_id=ID(62),
+
+control = Control(get_schema_a(), train, test, compile_models=False, max_id=ID(52),
 	accuracy_function=lambda x, y: torch.sum((x > 0.5) == y).item() / len(y))
-control.search([LockedShape(CLASS_SIZE, review_length)], "./temp_saves", torch.nn.BCEWithLogitsLoss(),
+control.search([LockedShape(CLASS_SIZE, TWEET_LENGTH)], "./temp_saves", torch.nn.BCEWithLogitsLoss(),
 	workers=4, batch_size=64, model_pool_size=5, training_epochs=30, breed_iterations=10, validation_multiple=5)
 
