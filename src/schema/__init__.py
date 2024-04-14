afe57b1eb3013e6e5f243ff5c74d4ec213551ae5
iainquayle <iainquayle@outlook.com>
2024-04-14 01:15:49 -0600

    new compilation working

--- src/schema/__init__.py
+++ src/schema/__init__.py
@@ -1,6 +1,6 @@
-from .ir_node import IRNode 
-from .schema_graph import SchemaNode, Transition, TransitionGroup, JoinType
-from .schema import Schema
-from .compile_indices import BreedIndices, SequenceIndices 
-from .compile_index import CompileIndex 
-from .growth_functions import *
+#from .ir_node import IRNode 
+#from .schema_graph import SchemaNode, Transition, TransitionGroup 
+#from .schema import Schema
+#from .compile_indices import BreedIndices, SequenceIndices 
+#from .compile_index import CompileIndex 
+#from .growth_functions import *
--- tests/compile/ir_compilation.py
+++ tests/compile/ir_compilation.py
@@ -1,7 +1,7 @@
 import unittest
 
-from src.schema import SchemaNode, JoinType 
-from src.schema.compile import NodeTracker, NodeTrackerStack, CompilationTracker
+from src.schema.schema_graph import *  
+#from src.schema.compile import NodeTracker, NodeTrackerStack, CompilationTracker
 from src.schema.components import Concat, Sum, Conv, ReLU, BatchNorm, Full
 from src.schema.compile_indices import BreedIndices
 from src.shared import *
@@ -11,31 +11,34 @@ class TestCompilation(unittest.TestCase):
 		start_schema = SchemaNode(ShapeBound((1, 10)), None, None, None, None, None, 1, "start")
 		mid_schema = SchemaNode(ShapeBound((1, 10)), None, None, None, None, None, 1, "mid")
 		end_schema = SchemaNode(ShapeBound((1, 10)), None, Concat(), None, None, None, 1, "end")
-		start_schema.add_group((mid_schema, 0, JoinType.NEW), (end_schema, 1, JoinType.NEW))
-		mid_schema.add_group((end_schema, 0, JoinType.EXISTING))
+		start_schema.add_group(New(mid_schema, 0), New(end_schema, 1))
+		mid_schema.add_group(Existing(end_schema, 0))
 		tracker = CompilationTracker([NodeTrackerStack(start_schema, [NodeTracker(set(), [], LockedShape(5), 0)])], None, ID(0), ID(100)) 
-		nodes = tracker.compile_ir(BreedIndices())
+		schema, node = tracker.pop_min()
+		nodes = schema.compile(node, tracker, BreedIndices(), ID(0), ID(5))
 		if nodes is None:
 			self.fail()
 		self.assertEqual(len(nodes), 3)
 	def test_divisor_hint(self):
 		start_schema = SchemaNode(ShapeBound((1, 10), 1), None, None, Conv(), None, None, 1, "start")
-		hinted = SchemaNode(ShapeBound((1, 10), 1), None, None, None, None, None, 2, "mid")
+		hinted = SchemaNode(ShapeBound((1, 10), 1), None, None, None, None, None, 2, "hinted")
 		end = SchemaNode(ShapeBound((1, 10), 1), None, None, Conv(groups=2), None, None, 1, "end")
-		start_schema.add_group((hinted, 0, JoinType.NEW))
-		hinted.add_group((end, 0, JoinType.NEW))
+		start_schema.add_group(New(hinted, 0))
+		hinted.add_group(New(end, 0))
 		tracker = CompilationTracker([NodeTrackerStack(start_schema, [NodeTracker(set(), [], LockedShape(1, 1), 0)])], None, ID(0), ID(100)) 
-		nodes = tracker.compile_ir(BreedIndices())
+		schema, node = tracker.pop_min()
+		nodes = schema.compile(node, tracker, BreedIndices(), ID(0), ID(5))
 		if nodes is None:
 			self.fail()
 		self.assertEqual(len(nodes), 3)
 	def test_loop(self):
 		main = SchemaNode(ShapeBound((1, 1), (1, 16)), None, None, Conv(kernel=2, stride=2), None, None, 1, "main")
 		end = SchemaNode(ShapeBound((1, 1), (1, 1)), None, None, None, None, None, 1, "end")
-		main.add_group((end, 0, JoinType.NEW))
-		main.add_group((main, 0, JoinType.NEW))
+		main.add_group(New(end, 0))
+		main.add_group(New(main, 0))
 		tracker = CompilationTracker([NodeTrackerStack(main, [NodeTracker(set(), [], LockedShape(1, 8), 0)])], None, ID(0), ID(100)) 
-		nodes = tracker.compile_ir(BreedIndices())
+		schema, node = tracker.pop_min()
+		nodes = schema.compile(node, tracker, BreedIndices(), ID(0), ID(5))
 		if nodes is None:
 			self.fail()
 		self.assertEqual(len(nodes), 4)
@@ -54,12 +57,13 @@ class TestCompilation(unittest.TestCase):
 			ReLU(), 
 			BatchNorm(), 1, "split_2")
 		end_node = SchemaNode( ShapeBound((1, 1), (1, 1)), None, None, Full(), None, None, 1, "end")
-		main.add_group( (split_1, 0, JoinType.NEW), (split_2, 1, JoinType.NEW))
-		split_1.add_group( (main, 2, JoinType.NEW))
-		split_2.add_group( (main, 2, JoinType.EXISTING))
-		main.add_group( (end_node, 0, JoinType.NEW))
+		main.add_group( New(split_1, 0), New(split_2, 1))
+		split_1.add_group( New(main, 2))
+		split_2.add_group( Existing(main, 2))
+		main.add_group( New(end_node, 0))
 		tracker = CompilationTracker([NodeTrackerStack(main, [NodeTracker(set(), [], LockedShape(1, 8), 0)])], None, ID(0), ID(100)) 
-		nodes = tracker.compile_ir(BreedIndices())
+		schema, node = tracker.pop_min()
+		nodes = schema.compile(node, tracker, BreedIndices(), ID(0), ID(12))
 		if nodes is None:
 			self.fail()
 		self.assertEqual(len(nodes), 11)
