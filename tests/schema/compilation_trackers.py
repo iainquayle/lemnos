f5151cd882b0c86b45867e141f5ed2a624276412
iainquayle <iainquayle@outlook.com>
2024-05-24 03:06:43 +0000

    fix broken tests

--- tests/schema/compilation_trackers.py
+++ tests/schema/compilation_trackers.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.schema.schema_graph import SchemaNode, New, Existing 
+from src.schema.schema_graph import SchemaNode, _CompilationNodeStack, _CompilationTracker, _CompilationNode
 from src.schema.components import Concat 
 from src.shared import *
 
@@ -11,19 +11,14 @@ class TestStack(unittest.TestCase):
 		self.s3 = SchemaNode(ShapeBound(1), None, Concat())
 		self.s4 = SchemaNode(ShapeBound(1), None, Concat())
 	def test_record(self):
-		stack1 = NodeTrackerStack(self.s1, [])
-		self.assertRaises(ValueError, stack1.next, self.s2, JoinType.EXISTING, LockedShape(1), 0, 0)
-		stack2 = stack1.next(self.s2, JoinType.NEW, LockedShape(1), ID(0), 0)
-		self.assertEqual(len(stack1), 0)
-		self.assertEqual(len(stack2), 1)
-		stack3 = stack2.next(self.s2, JoinType.NEW, LockedShape(1), ID(1), 0)
-		stack4 = stack3.next(self.s3, JoinType.EXISTING, LockedShape(1), ID(2), 0)
-		self.assertEqual(len(stack3._stack[1].parent_ids), 1)
-		self.assertEqual(len(stack4._stack[1].parent_ids), 2)
-		self.assertEqual(len(stack4._stack[0].parent_ids), 1)
-		self.assertEqual(stack4._stack[1].input_shape, LockedShape(2))
-		stack5 = stack4.next(self.s3, JoinType.EXISTING, LockedShape(1), ID(3), 0)
-		self.assertEqual(len(stack5._stack[0].parent_ids), 2)
+		stack1 = _CompilationNodeStack(self.s1, [])
+		self.assertIsNone(stack1.join_existing(self.s2, LockedShape(1), ID(0), 0))
+		stack1.join_new(self.s2, LockedShape(1), ID(0), 0)
+		self.assertEqual(len(stack1), 1)
+		self.assertEqual(len(stack1._stack[0].parent_ids), 1)
+		self.assertIsNone(stack1.join_existing(self.s2, LockedShape(1), ID(0), 0))
+		stack1.join_existing(self.s3, LockedShape(1), ID(1), 0)
+		self.assertEqual(len(stack1._stack[0].parent_ids), 2)
 
 class TestTracker(unittest.TestCase):
 	def setUp(self):
