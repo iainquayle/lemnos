9e05abecc6086049cb4363e98ead559030c3ea42
iainquayle <iainquayle@outlook.com>
2024-05-31 17:52:01 -0600

    refactor control

--- src/adapter/torch/runner.py
+++ src/adapter/torch/runner.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 from ...schema import IRNode
-from ...control import Runner, RunnerBuilder, EpochMetrics
+from ...control import Runner 
 from .formatter import DefaultComponentFormatter, TorchComponentFormatter, create_module 
 
 import torch
 from torch import Tensor
 from torch.nn import Module
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, IterableDataset, Dataset
 
 from typing import Callable, Any
 
@@ -67,56 +67,60 @@ class TorchRunnerBuilder(RunnerBuilder):
 		self._compiler_backend = compiler_backend
 		self._optimizer = optimizer 
 	def build(self, ir: list[IRNode]) -> Runner:
-		runnable_model: Any = get_module(f"Model", ir, DefaultComponentFormatter())
+		runnable_model: Any = create_module(f"Model", ir, DefaultComponentFormatter())
 		if self._compiler_backend is not None:
 			runnable_model = torch.compile(runnable_model, backend=str(self._compiler_backend)) 
 		runnable_model.to(self._device)
 		optimizer = self._optimizer.get(runnable_model)
 		return TorchRunner(runnable_model, self._train_loader, self._validation_loader, optimizer, self._criterion, self._accuracy_function, self._device, self._device_type)
 
-class TorchRunner(Runner):
+#change these to be just one? seems like a lot of work otherwise?
+class TrainOnly(Runner):
 	def __init__(self, 
-			model: Module, 
-			train_loader: DataLoader, 
-			validation_loader: DataLoader, 
-			optimizer: torch.optim.Optimizer,
+			formatter: TorchComponentFormatter,
+			data_loader: DataLoader,
 			criterion: Module,
-			accuracy_function: AccuracyFunction,
+			accuracy_function: AccuracyFunction | None,
+			lr: float,
+			optimizer: Optimizer,
 			device: torch.device,
-			device_type: str
+			require_cuda: bool,
+			torch_compiler: CompileBackend | None = None,
 		) -> None:
-		self._model = model
-		self._train_loader = train_loader
-		self._validation_loader = validation_loader
-		self._optimizer = optimizer
+		self._device_type = CUDA if torch.cuda.is_available() else CPU 
+		if require_cuda and not self._device_type == CUDA:
+			raise ValueError("CUDA not available")
+		self._formatter = formatter
+		self._data_loader = data_loader 
 		self._criterion = criterion
 		self._accuracy_function = accuracy_function
+		self._lr = lr
+		self._optimizer = optimizer
 		self._device = device
-		self._device_type = device_type
-	def train_epoch(self) -> EpochMetrics:
-		return train_epoch(self._model, self._criterion, self._accuracy_function, self._optimizer, self._train_loader, self._device, self._device_type)
-	def validate_epoch(self) -> EpochMetrics:
-		return validate_epoch(self._model, self._criterion, self._accuracy_function, self._validation_loader, self._device, self._device_type)
-
-
-def train_epoch(model: Module, criterion: Module, accuracy_function: AccuracyFunction, optimizer: torch.optim.Optimizer, train_loader: DataLoader, device: torch.device, device_type: str) -> EpochMetrics:
-	metrics: EpochMetrics = EpochMetrics()
-	model.train()
-	scaler = torch.cuda.amp.GradScaler()
-	for i, (data, truth) in enumerate(train_loader):
-		data, truth = data.to(device), truth.to(device)
-		optimizer.zero_grad(set_to_none=True)
-		with torch.autocast(device_type=device_type, dtype=torch.float16):
-			output = model(data)
-			loss = criterion(output, truth)
-			accuracy = accuracy_function(output, truth)
-			metrics.record(loss.item(), accuracy)
-		scaler.scale(loss).backward()
-		scaler.step(optimizer)
-		scaler.update()
-	print(metrics)
-	gc.collect()
-	return metrics 
+		self._torch_compiler = torch_compiler
+	def evaluate_model(self, ir: list[IRNode]) -> float:
+		model: Any = create_module("Model", ir, self._formatter)
+		if self._torch_compiler is not None:
+			model = torch.compile(model, backend=str(self._torch_compiler))
+		model.to(self._device)
+		optimizer = self._optimizer.get(model)
+		model.train()
+		scaler = torch.cuda.amp.GradScaler()
+		for (data, truth) in self._data_loader:
+			data, truth = data.to(self._device), truth.to(self._device)
+			optimizer.zero_grad(set_to_none=True)
+			with torch.autocast(device_type=self._device_type, dtype=torch.float16):
+				output = model(data)
+				loss = self._criterion(output, truth)
+				if self._accuracy_function is not None:
+					accuracy = self._accuracy_function(output, truth)
+			scaler.scale(loss).backward()
+			scaler.step(optimizer)
+			scaler.update()
+		return 0
+		
+class TrainAndValidate(Runner):
+	pass
 
 def validate_epoch(model: Module, criterion: Module, accuracy_function: AccuracyFunction, validation_loader: DataLoader, device: torch.device, device_type: str) -> EpochMetrics:
 	metrics: EpochMetrics = EpochMetrics()
