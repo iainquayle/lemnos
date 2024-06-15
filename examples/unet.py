5b07de6b09cb9b299eb21b21ccb492791062abfa
Iain Quayle <iainquayle@outlook.com>
2024-06-15 16:19:50 -0400

    notes and start examples

---
+++ examples/unet.py
@@ -0,0 +1,10 @@
+#
+# NOTE: this only demonstrates how to make a resnet like search space, not the use of a search controller
+# To see the use of a controller, go to the mnist example.
+#
+
+from __future__ import annotations
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
