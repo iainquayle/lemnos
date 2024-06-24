934434551392d9f4e2cb50fddb70bb68c6006bd3
iainquayle <iainquayle@outlook.com>
2024-06-24 13:46:00 -0400

    fix mixconv, add temp logging

--- tests/adapter/torch_compilation.py
+++ tests/adapter/torch_compilation.py
@@ -67,6 +67,7 @@ class TestTorchModule(unittest.TestCase):
 		ir = schema.compile_ir([LockedShape(1, 8)], BreedIndices(), ID(15))
 		if ir is None:
 			self.fail()
+		print(generate_source("Test", ir))
 		module = create_module("test", ir)
 		input = torch.ones(2, 1, 8)
 		self.assertEqual(module(input).shape, torch.Size([2, 1]))
