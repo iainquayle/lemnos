7db225ce74a5afe681e1ee3f98569639c08a06df
iainquayle <iainquayle@outlook.com>
2024-06-28 10:03:18 -0400

    scheduler added

--- lemnos/adapter/torch/__init__.py
+++ lemnos/adapter/torch/__init__.py
@@ -1,2 +1,2 @@
 from .formatter import create_module, generate_source, TorchComponentFormatter, DefaultComponentFormatter
-from .evaluator import TorchEvaluator, Adam, SGD, Optimizer
+from .evaluator import TorchEvaluator, Adam, SGD, Optimizer, Scheduler, StepLR, OneCycleLR, AccuracyFunction
