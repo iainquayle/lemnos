6df6094bae72903996b084a14b5b97a996d47a1a
iainquayle <iainquayle@outlook.com>
2024-06-29 20:31:24 +0000

    cifar example updates

--- examples/cifar.py
+++ examples/cifar.py
@@ -12,14 +12,15 @@ from torch import nn
 
 from lemnos.shared import LockedShape, ShapeBound
 from lemnos.schema import Schema, SchemaNode, New, Existing, PowerGrowth, LinearGrowth, BreedIndices
-from lemnos.schema.components import Conv, BatchNorm, SiLU, Softmax, SiLU, Sum, GroupType, Full, LayerNorm, ChannelDropout
-from lemnos.adapter.torch import TorchEvaluator, generate_source, Adam, SGD, OneCycleLR
+from lemnos.schema.components import Conv, BatchNorm, Softmax, ReLU, SiLU, Sum, GroupType, Full, LayerNorm, ChannelDropout
+from lemnos.adapter.torch import TorchEvaluator, generate_source, Adam, SGD, StepLR 
 from lemnos.control import or_search, AvgLossWindowSelector 
 
 def main():
 	if (ir := model_1().compile_ir([LockedShape(3, 32, 32)], BreedIndices(), 70)) is not None: #purely for demonstration purposes
 		print(generate_source("Example", ir))
-		train_transform = transforms.Compose([transforms.ToTensor(), transforms.RandomHorizontalFlip(p=.5), transforms.RandomErasing(p=.4, scale=(.02, .2)), transforms.RandomVerticalFlip(p=.5)])
+		#train_transform = transforms.Compose([transforms.ToTensor(), transforms.RandomHorizontalFlip(p=.5), transforms.RandomErasing(p=.4, scale=(.02, .2)), transforms.RandomVerticalFlip(p=.5)])
+		train_transform = transforms.Compose([transforms.ToTensor(), transforms.RandomHorizontalFlip(p=.5)])
 
 		train_data = datasets.CIFAR10('data', train=True, download=True, transform=train_transform)
 		validation_data = datasets.CIFAR10('data', train=False, download=True, transform=transforms.ToTensor())
@@ -28,7 +29,7 @@ def main():
 		validation_loader = DataLoader(validation_data, batch_size=64, shuffle=False, pin_memory=True, num_workers=1, persistent_workers=True, prefetch_factor=16)
 
 		accuracy_func = lambda x, y: (x.argmax(dim=1) == y).float().sum().item()
-		evaluator = TorchEvaluator(train_loader, validation_loader, 20, nn.CrossEntropyLoss(), accuracy_func, Adam(0.0003), OneCycleLR(.001, 20), True)
+		evaluator = TorchEvaluator(train_loader, validation_loader, 5, nn.CrossEntropyLoss(), accuracy_func, Adam(0.0003), None, True)
 
 		train_metrics, validation_metrics = evaluator.evaluate(ir)
 		#model_pool = or_search(model_1(), evaluator, AvgLossWindowSelector(1024), 80, 3, 3) 
@@ -39,20 +40,20 @@ def main():
 def model_1() -> Schema:
 	groups = 16 
 
-	head_1 = SchemaNode(ShapeBound(48, None, None), None, None, Conv(3, 1), SiLU(), BatchNorm())
-	head_2 = SchemaNode(ShapeBound(128, None, None), None, None, Conv(3, 1), SiLU(), BatchNorm())
+	head_1 = SchemaNode(ShapeBound(48, None, None), None, None, Conv(3, 1), ReLU(), BatchNorm())
+	head_2 = SchemaNode(ShapeBound(128, None, None), None, None, Conv(3, 1), ReLU(), BatchNorm())
 
 	head_1.add_group(New(head_2, 0))
 
 	accume = SchemaNode(ShapeBound(None, None, None), None, Sum(), None, None, BatchNorm() , debug_name="accume")
-	skip = SchemaNode(ShapeBound(None, None, None), None, None, None, None, ChannelDropout(.3), debug_name="skip")
+	skip = SchemaNode(ShapeBound(None, None, None), None, None, None, None, ChannelDropout(.4), debug_name="skip")
 	downsample = SchemaNode(ShapeBound(None, (2, None), (2, None)), PowerGrowth(256, .7, .0), Sum(), Conv(2, 0, 2, 1, groups, mix_groups=True), SiLU(), BatchNorm(), debug_name="downsample")
 
-	dw_3_point = SchemaNode(ShapeBound(None, None, None), LinearGrowth(2, .0), None, Conv(groups=groups, mix_groups=True), SiLU(), BatchNorm(), debug_name="dw_3_point")
-	depthwise_3 = SchemaNode(ShapeBound(None, None, None), None, None, Conv(3, 1, 1, 1, GroupType.DEPTHWISE), SiLU(), BatchNorm(), debug_name="depthwise_3")
+	dw_3_point = SchemaNode(ShapeBound(None, None, None), LinearGrowth(2, .0), None, Conv(groups=groups, mix_groups=True), ReLU(), BatchNorm(), debug_name="dw_3_point")
+	depthwise_3 = SchemaNode(ShapeBound(None, None, None), None, None, Conv(3, 1, 1, 1, GroupType.DEPTHWISE), ReLU(), BatchNorm(), debug_name="depthwise_3")
 	dw_collect = SchemaNode(ShapeBound(None, None, None), None, None, Conv(groups=groups, mix_groups=True), None, BatchNorm(), debug_name="dw_collect")
 
-	tail_1 = SchemaNode(ShapeBound(512, 1), None, None, Conv(2, 0), SiLU(), BatchNorm(), debug_name="tail_1")
+	tail_1 = SchemaNode(ShapeBound(256, 1), None, None, Conv(2, 0), ReLU(), BatchNorm(), debug_name="tail_1")
 	tail_2 = SchemaNode(ShapeBound(10, 1), None, None, Full(), Softmax(), None)
 
 	head_2.add_group(New(skip, 1), New(dw_3_point, 0))
@@ -76,14 +77,14 @@ def model_1() -> Schema:
 def model_2() -> Schema:
 	groups = 1 
 
-	head_1 = SchemaNode(ShapeBound(32, None, None), None, None, Conv(3, 1), SiLU(), BatchNorm())
-	head_2 = SchemaNode(ShapeBound(64, None, None), None, None, Conv(3, 1), SiLU(), BatchNorm())
+	head_1 = SchemaNode(ShapeBound(32, None, None), None, None, Conv(3, 1), ReLU(), BatchNorm())
+	head_2 = SchemaNode(ShapeBound(64, None, None), None, None, Conv(3, 1), ReLU(), BatchNorm())
 
-	downsample = SchemaNode(ShapeBound(None, (2, None), (2, None)), PowerGrowth(256, .7, .0), Sum(), Conv(2, 0, 2, 1, groups, mix_groups=True), SiLU(), BatchNorm())
+	downsample = SchemaNode(ShapeBound(None, (2, None), (2, None)), PowerGrowth(128, .7, .0), Sum(), Conv(2, 0, 2, 1, groups, mix_groups=True), ReLU(), BatchNorm())
 
-	conv_3 = SchemaNode(ShapeBound(None, None, None), None, None, Conv(3, 1, groups=groups, mix_groups=True), SiLU(), BatchNorm())
+	conv_3 = SchemaNode(ShapeBound(None, None, None), None, None, Conv(3, 1, groups=groups, mix_groups=True), ReLU(), BatchNorm())
 
-	tail_1 = SchemaNode(ShapeBound(128, 1), None, None, Conv(2, 0), SiLU(), BatchNorm())
+	tail_1 = SchemaNode(ShapeBound(128, 1), None, None, Conv(2, 0), ReLU(), BatchNorm())
 	tail_2 = SchemaNode(ShapeBound(10, 1), None, None, Full(), Softmax(), None)
 
 	head_1.add_group(New(head_2, 0))
