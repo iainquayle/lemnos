2a61d87f40999f3e59bc9df4c74b28515656ba47
iainquayle <iainquayle@outlook.com>
2024-09-16 00:14:54 -0400

    notes on scoping

--- lemnos/adapter/torch/formatters.py
+++ lemnos/adapter/torch/formatters.py
@@ -5,44 +5,60 @@ from ...schema.components import *
 
 from .formatter import InitType, InitStatement
 
-
 #need to be able to pass out the statement and the variable name
 
+from dataclasses import dataclass
 
-def conv_init(self: Conv, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+@dataclass
+class Statements:
+	init_statements: list[str]
+	intermediate_forward_statements: list[str]
+	output_forward_statement: str
+
+class IdentifierGenerator: #move this somewhere where it makes sense, probably in the adapter module to allow for reuse
+	def __init__(self, namespace: str):
+		self._namespace = namespace
+		self._identifiers: dict[str, int] = {} 
+	def get_identifier(self, name: str) -> str:
+		if name not in self._identifiers:
+			self._identifiers[name] = len(self._identifiers)
+		return f"{self._namespace}_{self._identifiers[name]}"
+
+
+def conv_formatter(self: Conv, input_shape: LockedShape, output_shape: LockedShape, **kwargs) -> Any:
 	return [InitStatement( InitType.CALLABLE, '',
 		nn_(f"Conv{len(input_shape) - 1}d({input_shape[0]}, {output_shape[0]}, {self._kernel}, {self._stride}, {self._padding}, {self._dilation}, {self._groups})"),
 	)]
-def maxpool_init(self: MaxPool, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def maxpool_formatter(self: MaxPool, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '',
 		nn_(f"MaxPool{len(input_shape) - 1}d({self._kernel}, {self._stride}, {self._padding}, {self._dilation})")
 	)]
-def full_init(self: Full, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def full_formatter(self: Full, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '',
 		nn_(f"Linear({input_shape.get_product()}, {output_shape.get_product()}, bias=True)")
 	)]
-def relu_init(self: ReLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def relu_formatter(self: ReLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("ReLU()"))]
-def relu6_init(self: ReLU6, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def relu6_formatter(self: ReLU6, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("ReLU6()"))]
-def silu_init(self: SiLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def silu_formatter(self: SiLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("SiLU()"))]
-def sigmoid_init(self: Sigmoid, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def sigmoid_formatter(self: Sigmoid, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("Sigmoid()"))]
-def softmax_init(self: Softmax, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def softmax_formatter(self: Softmax, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("Softmax(dim=1)"))]
-def batchnorm_init(self: BatchNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def batchnorm_formatter(self: BatchNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '',
 		nn_(f"BatchNorm{len(input_shape) - 1}d({input_shape[0]})")
 	)]
-def layernorm_init(self: LayerNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def layernorm_formatter(self: LayerNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	raise NotImplementedError
 	return [InitStatement( InitType.CALLABLE, '', nn_(f"LayerNorm({input_shape})"))]
-def dropout_init(self: Dropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def dropout_formatter(self: Dropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_(f"Dropout(p={self._probability})"))]
-def channel_dropout_init(self: ChannelDropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def channel_dropout_formatter(self: ChannelDropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_(f"ChannelDropout(p={self._probability})"))]
-def glu_init(self: GLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def glu_formatter(self: GLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
 	return [InitStatement( InitType.CALLABLE, '', nn_("GLU(dim=1)"))]
 
 
--- lemnos/schema/components/component.py
+++ lemnos/schema/components/component.py
@@ -4,28 +4,9 @@ from ...shared import LockedShape
 
 from abc import ABC as Abstract 
 
-from dataclasses import dataclass
+from typing import Any
 
-@dataclass
-class Statements:
-	init_statements: list[str]
-	intermediate_forward_statements: list[str]
-	output_forward_statement: str
-
-class IdentifierGenerator:
-	def __init__(self, namespace: str):
-		self._namespace = namespace
-		self._identifiers: dict[str, int] = {} 
-	def get_identifier(self, name: str) -> str:
-		if name not in self._identifiers:
-			self._identifiers[name] = len(self._identifiers)
-		return f"{self._namespace}_{self._identifiers[name]}"
 
 class Component(Abstract):
-	def get_statements(self, input_shape: LockedShape, output_shape: LockedShape, indentifier_generator: IdentifierGenerator) -> Statements:
+	def get_statements(self, input_shape: LockedShape, output_shape: LockedShape, **kwargs) -> Any:
 		raise NotImplementedError(f"must bind a statements generator for {self.__class__.__name__}")
-
-#would be best if both took in a name space generator maybe
-#	otherwise, would need to pass out a name for the members, and then that would need to be edited to add the namespace
-
-#the forwards need to take in the the input register? cant just have it done by the formatter anymore if there is a chance that it could get used in multiple spots...
