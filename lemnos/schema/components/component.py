a989242c5bb697045f4bcf853af8301d56c675aa
iainquayle <iainquayle@outlook.com>
2024-09-18 00:40:43 -0400

    update component statement formatter interface

--- lemnos/schema/components/component.py
+++ lemnos/schema/components/component.py
@@ -4,9 +4,14 @@ from ...shared import LockedShape
 
 from abc import ABC as Abstract 
 
-from typing import Any
+from typing import Any, Callable
 
+
+type Formatter = Callable[[Component, LockedShape, LockedShape, dict[str, Any]], Any]
 
 class Component(Abstract):
-	def get_statements(self, input_shape: LockedShape, output_shape: LockedShape, **kwargs) -> Any:
+	@classmethod
+	def attach_statement_generator(cls, formatter: Formatter) -> None:
+		cls.formatter = formatter
+	def get_statements(self, input_shape: LockedShape, output_shape: LockedShape, **kwargs: Any) -> Any:
 		raise NotImplementedError(f"must bind a statements generator for {self.__class__.__name__}")
