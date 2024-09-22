fca24d0489f1b31976243cb34451831fe7daae07
iainquayle <iainquayle@outlook.com>
2024-09-22 17:04:59 -0400

    generator interface implemented

--- lemnos/adapter/torch/standard_generators.py
+++ lemnos/adapter/torch/standard_generators.py
@@ -1,49 +1,59 @@
-from ...shared import LockedShape
+from __future__ import annotations
 
 from ...templates.torch import *
+from ...templates.python import *
 from ...schema.components import *
 
-from .generation import InitType, ComponentGeneratorData, ComponentGenerator
+from .generation import InitType, StatementGeneratorOutput, StatementGeneratorArgs, SourceGenerator
 
-#need to be able to pass out the statement and the variable name
 
-def conv_generator(self: Conv, input_shape: LockedShape, output_shape: LockedShape, register: str, **kwargs: Any) -> Any:
-	return [InitStatement( InitType.CALLABLE, '',
-		nn_(f"Conv{len(input_shape) - 1}d({input_shape[0]}, {output_shape[0]}, {self._kernel}, {self._stride}, {self._padding}, {self._dilation}, {self._groups})"),
-	)]
+def standard_module(module: str, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	identifier = args.member_identifier_generator.get_identifier()
+	init_statement = [assign_(self_(identifier), nn_(module))]
+	return_expression = call_(self_(identifier),  args.input_register)
+	return StatementGeneratorOutput(init_statement, [], return_expression)
 
-def maxpool_formatter(self: MaxPool, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '',
-		nn_(f"MaxPool{len(input_shape) - 1}d({self._kernel}, {self._stride}, {self._padding}, {self._dilation})")
-	)]
-def full_formatter(self: Full, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '',
-		nn_(f"Linear({input_shape.get_product()}, {output_shape.get_product()}, bias=True)")
-	)]
-def relu_formatter(self: ReLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("ReLU()"))]
-def relu6_formatter(self: ReLU6, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("ReLU6()"))]
-def silu_formatter(self: SiLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("SiLU()"))]
-def sigmoid_formatter(self: Sigmoid, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("Sigmoid()"))]
-def softmax_formatter(self: Softmax, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("Softmax(dim=1)"))]
-def batchnorm_formatter(self: BatchNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '',
-		nn_(f"BatchNorm{len(input_shape) - 1}d({input_shape[0]})")
-	)]
-def layernorm_formatter(self: LayerNorm, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
+def conv_generator(self: Conv, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"Conv{len(args.input_shape) - 1}d({args.input_shape[0]}, {args.output_shape[0]}, {self._kernel}, {self._stride}, {self._padding}, {self._dilation}, {self._groups})", args)
+
+def maxpool_generator(self: MaxPool, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"MaxPool{len(args.input_shape) - 1}d({self._kernel}, {self._stride}, {self._padding}, {self._dilation})", args)
+
+def full_generator(self: Full, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"Linear({args.input_shape.get_product()}, {args.output_shape.get_product()}, bias=True)", args)
+
+def relu_generator(self: ReLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("ReLU()", args)
+
+def relu6_generator(self: ReLU6, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("ReLU6()", args)
+
+def silu_generator(self: SiLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("SiLU()", args)
+
+def sigmoid_generator(self: Sigmoid, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("Sigmoid()", args)
+
+def softmax_generator(self: Softmax, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("Softmax(dim=1)", args)
+
+def batchnorm_generator(self: BatchNorm, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"BatchNorm{len(args.input_shape) - 1}d({args.input_shape[0]})", args)
+
+def layernorm_generator(self: LayerNorm, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
 	raise NotImplementedError
 	return [InitStatement( InitType.CALLABLE, '', nn_(f"LayerNorm({input_shape})"))]
-def dropout_formatter(self: Dropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_(f"Dropout(p={self._probability})"))]
-def channel_dropout_formatter(self: ChannelDropout, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_(f"ChannelDropout(p={self._probability})"))]
-def glu_formatter(self: GLU, input_shape: LockedShape, output_shape: LockedShape) -> list[InitStatement]:
-	return [InitStatement( InitType.CALLABLE, '', nn_("GLU(dim=1)"))]
 
+def dropout_generator(self: Dropout, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"Dropout(p={self._probability})", args)
 
-def conv_forward(self: Conv,  input_expr: str) -> str:
-	return nn_(f"Conv{len(self.input_shape) - 1}d({input_expr})")
+def channel_dropout_generator(self: ChannelDropout, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module(f"ChannelDropout(p={self._probability})", args)
+
+def glu_generator(self: GLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+	return standard_module("GLU(dim=1)", args)
+
+standard_generator = SourceGenerator({ 
+	Conv: conv_generator,
+	Full: full_generator,
+})
