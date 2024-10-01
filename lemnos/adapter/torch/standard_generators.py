393a9a729be76acd6106d8d4d4b99bfd3febcd28
iainquayle <iainquayle@outlook.com>
2024-10-01 06:14:50 +0000

    fix shape viewing side effect

--- lemnos/adapter/torch/standard_generators.py
+++ lemnos/adapter/torch/standard_generators.py
@@ -4,77 +4,77 @@ from ...templates.torch import *
 from ...templates.python import *
 from ...schema.components import *
 
-from .generator import InitType, StatementGeneratorOutput, StatementGeneratorArgs, SourceGenerator, ShapeView
+from .generator import InitType, ComponentStatements, StatementGeneratorArgs, SourceGenerator,  StatementGenerator, ShapeView
 
-def standard_module(module: str, args: StatementGeneratorArgs, view: ShapeView) -> StatementGeneratorOutput:
+def standard_module(module: str, args: StatementGeneratorArgs) -> ComponentStatements:
 	member_identifier = args.member_identifier_generator.get_identifier()
 	init_statement = [assign_(member_identifier, self_(nn_(module)))]
 	return_expression = call_(member_identifier,  args.input_registers[0])
-	return StatementGeneratorOutput(init_statement, [], return_expression, view)
+	return ComponentStatements(init_statement, [], return_expression)
 
-def sum_generator(self: Sum, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return StatementGeneratorOutput([], [], sum_(args.input_registers), ShapeView.FLAT)
+def sum_generator(self: Sum, args: StatementGeneratorArgs) -> ComponentStatements:
+	return ComponentStatements([], [], sum_(args.input_registers), )
 
-def concat_generator(self: Concat, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return StatementGeneratorOutput([], [], self_(concat_(args.input_registers)), ShapeView.FLAT)
+def concat_generator(self: Concat, args: StatementGeneratorArgs) -> ComponentStatements:
+	return ComponentStatements([], [], self_(concat_(args.input_registers)), )
 
-def conv_generator(self: Conv, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+def conv_generator(self: Conv, args: StatementGeneratorArgs) -> ComponentStatements:
 	dimensionality = len(args.input_shape) - 1
-	return standard_module(f"Conv{dimensionality}d({args.input_shape[0]}, {args.output_shape[0]}, {self._kernel.expand(dimensionality)}, {self._stride.expand(dimensionality)}, {self._padding.expand(dimensionality)}, {self._dilation.expand(dimensionality)}, {self._groups})", args, ShapeView.REAL)
+	return standard_module(f"Conv{dimensionality}d({args.input_shape[0]}, {args.output_shape[0]}, {self._kernel.expand(dimensionality)}, {self._stride.expand(dimensionality)}, {self._padding.expand(dimensionality)}, {self._dilation.expand(dimensionality)}, {self._groups})", args, )
 
-def maxpool_generator(self: MaxPool, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+def maxpool_generator(self: MaxPool, args: StatementGeneratorArgs) -> ComponentStatements:
 	dimensionality = len(args.input_shape) - 1
-	return standard_module(f"MaxPool{dimensionality}d({self._kernel.expand(dimensionality)}, {self._stride.expand(dimensionality)}, {self._padding.expand(dimensionality)}, {self._dilation.expand(dimensionality)})", args, ShapeView.REAL)
+	return standard_module(f"MaxPool{dimensionality}d({self._kernel.expand(dimensionality)}, {self._stride.expand(dimensionality)}, {self._padding.expand(dimensionality)}, {self._dilation.expand(dimensionality)})", args, )
 
-def full_generator(self: Full, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module(f"Linear({args.input_shape.get_product()}, {args.output_shape.get_product()}, bias=True)", args, ShapeView.FLAT)
+def full_generator(self: Full, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module(f"Linear({args.input_shape.get_product()}, {args.output_shape.get_product()}, bias=True)", args, )
 
-def relu_generator(self: ReLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module("ReLU()", args, ShapeView.EITHER)
+def relu_generator(self: ReLU, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module("ReLU()", args, )
 
-def relu6_generator(self: ReLU6, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module("ReLU6()", args, ShapeView.EITHER)
+def relu6_generator(self: ReLU6, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module("ReLU6()", args, )
 
-def silu_generator(self: SiLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module("SiLU()", args, ShapeView.EITHER)
+def silu_generator(self: SiLU, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module("SiLU()", args, )
 
-def sigmoid_generator(self: Sigmoid, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module("Sigmoid()", args, ShapeView.EITHER)
+def sigmoid_generator(self: Sigmoid, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module("Sigmoid()", args, )
 
-def softmax_generator(self: Softmax, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module("Softmax(dim=1)", args, ShapeView.EITHER)
+def softmax_generator(self: Softmax, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module("Softmax(dim=1)", args, )
 
-def batchnorm_generator(self: BatchNorm, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module(f"BatchNorm{len(args.input_shape) - 1}d({args.input_shape[0]})", args, ShapeView.REAL)
+def batchnorm_generator(self: BatchNorm, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module(f"BatchNorm{len(args.input_shape) - 1}d({args.input_shape[0]})", args, )
 
-def layernorm_generator(self: LayerNorm, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+def layernorm_generator(self: LayerNorm, args: StatementGeneratorArgs) -> ComponentStatements:
 	raise NotImplementedError
 	return [InitStatement( InitType.CALLABLE, '', nn_(f"LayerNorm({input_shape})"))]
 
-def dropout_generator(self: Dropout, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module(f"Dropout(p={self._probability})", args, ShapeView.EITHER)
+def dropout_generator(self: Dropout, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module(f"Dropout(p={self._probability})", args, )
 
-def channel_dropout_generator(self: ChannelDropout, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
-	return standard_module(f"ChannelDropout(p={self._probability})", args, ShapeView.REAL)
+def channel_dropout_generator(self: ChannelDropout, args: StatementGeneratorArgs) -> ComponentStatements:
+	return standard_module(f"ChannelDropout(p={self._probability})", args, )
 
-def glu_generator(self: GLU, args: StatementGeneratorArgs) -> StatementGeneratorOutput:
+def glu_generator(self: GLU, args: StatementGeneratorArgs) -> ComponentStatements:
 	raise NotImplementedError
-	return standard_module("GLU(dim=1)", args, ShapeView.REAL)
+	return standard_module("GLU(dim=1)", args, )
 
 standard_generator = SourceGenerator({ 
-	Concat: concat_generator,
-	Sum: sum_generator,
-	Conv: conv_generator,
-	Full: full_generator,
-	MaxPool: maxpool_generator,
-	ReLU: relu_generator,
-	ReLU6: relu6_generator,
-	SiLU: silu_generator,
-	Sigmoid: sigmoid_generator,
-	Softmax: softmax_generator,
-	BatchNorm: batchnorm_generator,
-	LayerNorm: layernorm_generator,
-	Dropout: dropout_generator,
-	ChannelDropout: channel_dropout_generator,
-	GLU: glu_generator
+	Concat: StatementGenerator(concat_generator, ShapeView.FLAT),
+	Sum: StatementGenerator(sum_generator, ShapeView.FLAT),
+	Conv: StatementGenerator(conv_generator, ShapeView.REAL),
+	Full: StatementGenerator(full_generator, ShapeView.FLAT),
+	MaxPool: StatementGenerator(maxpool_generator, ShapeView.REAL),
+	ReLU: StatementGenerator(relu_generator, ShapeView.EITHER),
+	ReLU6: StatementGenerator(relu6_generator, ShapeView.EITHER),
+	SiLU: StatementGenerator(silu_generator, ShapeView.EITHER),
+	Sigmoid: StatementGenerator(sigmoid_generator, ShapeView.EITHER),
+	Softmax: StatementGenerator(softmax_generator, ShapeView.EITHER),
+	BatchNorm: StatementGenerator(batchnorm_generator, ShapeView.REAL),
+	LayerNorm: StatementGenerator(layernorm_generator, ShapeView.REAL),
+	Dropout: StatementGenerator(dropout_generator, ShapeView.EITHER),
+	ChannelDropout: StatementGenerator(channel_dropout_generator, ShapeView.REAL),
+	GLU: StatementGenerator(glu_generator, ShapeView.REAL),
 })
