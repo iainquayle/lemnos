0355ef4b5c610e33ac067923ad877862931e6f1a
iainquayle <iainquayle@outlook.com>
2024-10-06 00:02:09 +0000

    init setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+	name='lemnos',
+	version='0.1',
+	packages=find_packages(),
+	install_requires=[], #move requirements to here...
+	author='Iain Quayle',
+	#author_email='',
+	#description='',
+	#long_description='' #open('README.md').read(),
+	#long_description_content_type='text/markdown',
+	#url='',
+	python_requires='>=3.11',
+)
