244c8a377be0cede881c8e10adf53d2813d7e089
iainquayle <iainquayle@outlook.com>
2025-01-14 23:16:18 -0500

    rename lemnos torch for imports, fix setup.py

--- torch/setup.py
+++ torch/setup.py
@@ -1,21 +1,23 @@
 from setuptools import setup, find_packages
 
-lemnos_address = 'https://github/iainquayle/lemnos.git'
+lemnos_address = 'https://github.com/iainquayle/lemnos.git'
+lemnos_branch = 'master'
 lemnos_name = 'lemnos'
+lemnos_dir = 'core'
 
 setup(
-	name='lemnos-torch',
+	name='lemnos_torch',
 	version='0.1',
 	packages=find_packages(),
 	install_requires=[
 		'torch',
 		'torchvision',
 		'typing_extensions',
-		f'{lemnos_name} @ {lemnos_address}#egg={lemnos_name}&subdirectory=core',
+		f'{lemnos_name} @ git+{lemnos_address}@{lemnos_branch}#egg={lemnos_name}&subdirectory={lemnos_dir}',
 	], 
-	dependency_links=[
-		lemnos_address
-	],
+	#dependency_links=[
+	#	lemnos_address
+	#],
 	author='Iain Quayle',
 	#author_email='',
 	#description='',
